d1dbca7835fd86083b0640fc21b9f11d0d058373
Florian Finkernagel <TyberiusPrime@users.noreply.github.com>
2019-04-17 14:16:52 +0000

    removed unused code

--- src/mbf_genomes/__init__.py
+++ src/mbf_genomes/__init__.py
@@ -3,13 +3,11 @@ from .base import GenomeBase, HardCodedGenome
 from .ensembl import EnsemblGenome
 from .filebased import FileBasedGenome, InteractiveFileBasedGenome
 
-data_path = Path(__file__).parent.parent.parent / "data"
 
 __all__ = [
     "GenomeBase",
     "EnsemblGenome",
     "FileBasedGenome",
     "InteractiveFileBasedGenome",
-    "data_path",
     "HardCodedGenome",
 ]
