12cbf04eecd01e65b4837a028ebc490e7d943824
Florian Finkernagel <TyberiusPrime@users.noreply.github.com>
2019-08-22 11:53:33 +0000

    Merged

--- src/mbf_genomes/base.py
+++ src/mbf_genomes/base.py
@@ -633,16 +633,27 @@ class GenomeBase(ABC):
         return res
 
     def get_genes_overlapping(self, chr, start, stop):
-        check_overlap = lambda df, interval: np.max(
-            [
-                np.zeros(len(df)),
-                np.min([df.stop.values, np.ones(len(df), dtype=int) * interval[1]], axis=0)
-                - np.max([df.start.values, np.ones(len(df), dtype=int) * interval[0]], axis=0),
-            ],
-            axis=0,
+        def check_overlap(df, interval):
+            return np.max(
+                [
+                    np.zeros(len(df)),
+                    np.min(
+                        [df.stop.values, np.ones(len(df), dtype=int) * interval[1]],
+                        axis=0,
+                    )
+                    - np.max(
+                        [df.start.values, np.ones(len(df), dtype=int) * interval[0]],
+                        axis=0,
+                    ),
+                ],
+                axis=0,
+            )
+
+        filter = (self.df_genes["chr"] == chr) & (
+            check_overlap(self.df_genes, [start, stop]) > 0
         )
-        filter = (self.df_genes["chr"] == chr) & (check_overlap(self.df_genes, [start, stop]) > 0)
         return self.df_genes[filter]
+
 
 @class_with_downloads
 class HardCodedGenome(GenomeBase):
